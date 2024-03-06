631f9dc5a4aa24a60e70bfc992eec469e4bb15de
40284751 <nwhaley01@qub.ac.uk>
2024-03-06 14:07:28 +0000

    Add range test code

--- lab2/Lab2/src/org/jfree/data/test/RangeTest.java
+++ lab2/Lab2/src/org/jfree/data/test/RangeTest.java
@@ -12,18 +12,102 @@ public class RangeTest {
 	private Range rangeObjectUnderTest;
 	
 	@Before
-	public void setUp() throws Exception {
-		rangeObjectUnderTest = new Range(-1, 1);
+	public void setUp() throws Exception {;
+		rangeObjectUnderTest = new Range(1, 5);
 	}
-
+	
 	@Test
-	public void testCentralValueShouldBeZero() {
-		assertEquals("The central value of -1 and 1 should be 0",
-					0, rangeObjectUnderTest.getCentralValue(), 0.000000001d);
+    public void testEqualsMethodReturnsTrueGivenIdenticalRangeObject() {
+		assertTrue("Failure: Returns False when should return True.", rangeObjectUnderTest.equals(new Range(1, 5)));
 	}
+	
+	@Test
+    public void testEqualsMethodReturnsFalseGivenNonIdenticalRangeObject() {
+		assertFalse("Failure: Returns True when should return False.", rangeObjectUnderTest.equals(new Range(1, 4)));
 
+	}
+	
+	@Test
+    public void testEqualsMethodReturnsFalseGivenDifferentJavaObject() {
+		assertFalse("Failure: Returns True when should return False.", rangeObjectUnderTest.equals(new String("test")));
+	}
+	
+	@Test
+    public void testEqualsMethodReturnsFalseGivenNullValue() {
+		assertFalse("Failure: Returns True when should return False.", rangeObjectUnderTest.equals(null));
+	}
+	
+	@Test
+    public void testConstrainMethodReturnsClosestValueGivenValueWithinRange() {
+		assertEquals("Failure: Returns incorrect value when should return 4.", 
+				4.0, rangeObjectUnderTest.constrain(4), 0.0000001d);
+	}
+	@Test
+    public void testConstrainMethodReturnsClosestValueGivenValueBelowRange() {
+		assertEquals("Failure: Returns incorrect value when should return 1.", 
+				1.0, rangeObjectUnderTest.constrain(-1), 0.0000001d);
+	}
+	@Test
+    public void testConstrainMethodReturnsClosestValueGivenValueAboveRange() {
+		assertEquals("Failure: Returns incorrect value when should return 5.", 
+				5.0, rangeObjectUnderTest.constrain(6), 0.0000001d);
+	}
+	
+	@Test
+    public void testConstrainMethodReturnsClosestValueGivenValueOnRangeBoundary() {
+		assertEquals("Failure: Returns incorrect value when should return 1.", 
+				1.0, rangeObjectUnderTest.constrain(1), 0.0000001d);
+	}
+	
+	@Test
+    public void testGetLowerBoundMethodReturnsCorrectOutputGivenRangeWithLowerLessThanUpper() {
+		assertEquals("Failure: Returns incorrect value when should return 1.", 
+				1.0, rangeObjectUnderTest.getLowerBound(), 0.0000001d);
+	}
+	
+	@Test
+    public void testGetLowerBoundMethodReturnsCorrectOutputGivenRangeWithLowerEqualToUpper() {
+		rangeObjectUnderTest = new Range(1, 1);
+		assertEquals("Failure: Returns incorrect value when should return 1.", 
+				1.0, rangeObjectUnderTest.getLowerBound(), 0.0000001d);
+	}
+	
+	@Test
+    public void testGetUpperBoundMethodReturnsCorrectOutputGivenRangeWithLowerLessThanUpper() {
+		assertEquals("Failure: Returns incorrect value when should return 5.", 
+				5.0, rangeObjectUnderTest.getUpperBound(), 0.0000001d);
+	}
+	
+	@Test
+    public void testGetUpperBoundMethodReturnsCorrectOutputGivenRangeWithLowerEqualToUpper() {
+		rangeObjectUnderTest = new Range(1, 1);
+		assertEquals("Failure: Returns incorrect value when should return 1.", 
+				1.0, rangeObjectUnderTest.getUpperBound(), 0.0000001d);
+	}
+	
+	@Test
+    public void testToStringMethodReturnsCorrectOutputGivenRangeWithLowerLessThanUpper() {
+		assertEquals("Failure: Returns incorrect value when should return Range[1.0,5.0].", 
+				"Range[1.0,5.0]", rangeObjectUnderTest.toString());
+	}
+	
+	@Test
+    public void testToStringMethodReturnsCorrectOutputGivenRangeWithLowerEqualToUpper() {
+		rangeObjectUnderTest = new Range(1, 1);
+		assertEquals("Failure: Returns incorrect value when should return Range[1.0,1.0].", 
+				"Range[1.0,1.0]", rangeObjectUnderTest.toString());
+	}
+	
+	@Test
+    public void testToStringMethodReturnsCorrectOutputGivenRangeWithNegativeNumbers() {
+		rangeObjectUnderTest = new Range(-5, -1);
+		assertEquals("Failure: Returns incorrect value when should return Range[-5.0,-1.0].", 
+				"Range[-5.0,-1.0]", rangeObjectUnderTest.toString());
+	}
+	
 	@After
 	public void tearDown() throws Exception {
+		rangeObjectUnderTest = null;
 
 	}
 
